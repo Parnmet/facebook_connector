1cdea533c4f651899cde30c51b9bd0a32526dc32
oato.o <oat9002@hotmail.com>
2016-11-29 17:32:41 +0700

    Added savePageFB and cronSavePage

--- service/FacebookService.js
+++ service/FacebookService.js
@@ -1,17 +1,27 @@
 import graph from 'fbgraph'
 import request from 'request'
+import cron from 'cron'
+import { db } from '../db'
 
 let access_token = "139610759813141|f928e2e59299981a997116c967a20b1d"
 graph.setAccessToken(access_token)
 
+
+const pageIDs = ['157556534255462']
+
+const cronJob = cron.CronJob
+
 export function getFbDetail(userID) {
-    var params = {fields: "name,picture"}    
+    var params = {fields: "name,picture"}
     return new Promise((resolve) =>{
         graph.get(userID,params, (err, res) => {
             resolve(res)
+            if(err != null) {
+              console.log(err)
+            }
         })
     })
-     
+
 }
 
 
@@ -24,22 +34,22 @@ export function getFbFeed(userID,since,until) {
         }
         else{
             params = {fields: "message,created_time",since: since,limit: 100}
-            
+
         }
     }
     else{
-        params = {fields: "message,created_time",limit: 100}        
+        params = {fields: "message,created_time",limit: 100}
     }
 
-    
+
     return new Promise((resolve,reject) => {
 
         graph.get(userID+"/feed",params,(err ,res) =>{
                 resolve(res)
         })
-       
+
     })
-     
+
 }
 
 export function getFbComment(postID){
@@ -52,3 +62,26 @@ export function getFbComment(postID){
     })
 
 }
+
+function saveFBpage(pageID) {
+  db.FB_PAGE.findOne({id: pageID}, (err, document) => {
+    if(!document) {
+      getFbDetail(pageID).then(page => {
+        db.FB_PAGE.insert(page, err => {
+          console.log(err)
+        })
+      })
+    }
+  })
+}
+
+const cronSaveFBpage = new cronJob('*/30 * * * * *', () => {
+  pageIDs.forEach(pageID => {
+    saveFBpage(pageID)
+  })
+},
+() => {
+  console.log('cronSaveFBpage has stopped.')
+},
+true
+)
